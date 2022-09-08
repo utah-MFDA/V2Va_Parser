b8c104d9dc1dc2bf8659593e576da304ad0fee68
Brady Goenner <brady.goenner@utah.edu>
2022-09-08 13:06:32 -0600

    init commit

---
+++ V2Va.py
@@ -0,0 +1,163 @@
+
+import numpy as np
+import pandas as pd
+import re
+
+inFile_Verilog = "smart_toilet.v"
+inFile_lengths = "smart_toilet_lengths.xlsx"
+library_csv =    "StandardCellLibrary.csv"
+outFile_sp     = inFile_Verilog[:-2] + '.sp'
+
+initExpress    = "initExpression_V2VA"
+endExpress     = "endExpression_V2VA"
+
+outfile_VA = inFile_Verilog[:-1] + 'va'
+
+Vfile = open(inFile_Verilog)
+SPfile= open(outFile_sp, '+w')
+iExp  = open(initExpress)
+eExp  = open(endExpress)
+
+# Load standard cell library
+
+library = pd.read_csv(library_csv)
+wireLenDF = pd.read_excel(inFile_lengths)
+
+SPfile.write(''.join(iExp.readlines()))
+
+numberOfComponents = 0
+currentLine = ''
+wireList    = {}
+outputWords = []
+
+
+# get line
+for line_num, line in enumerate(Vfile):
+
+    if len(line) > 0 and not(line == '\n'):
+        if not line.rstrip()[-1] == ';':
+            # append to current line
+            # remove comments
+            if '//' in line:
+                line = line.split('//')[0]
+            currentLine += line
+            continue
+        else:
+            # pass to parser
+            currentLine += line
+            line = currentLine
+    else:
+        continue
+
+    # remove inital whitespace
+    line = line.lstrip(' ').replace(';', '').replace('\n', '')
+    vars = line.split(' ')[0]
+ 
+    VA_line_str = ''
+
+
+    if vars == 'input':
+
+        # create pump devices
+        # we will assume pressure pumping devices
+        params = line.replace('input ', '').replace(' ', '').split(',')
+        for p in params:
+            VA_line_str += 'X' + str(numberOfComponents) + ' ' + str(p) + ' ' + str(p) + 'c PressurePump pressure=100k \n'
+            numberOfComponents += 1
+        
+        VA_line_str += '\n'
+
+    elif vars == 'output':
+        outputLine = line.replace('output ', '')
+        for out in line.replace(' ', '').split(','):
+            outputWords.append(out)
+
+    elif vars == 'module':
+        pass
+    
+    elif vars == 'wire':
+        # create channel modules
+        wires = line.replace('wire ', '').replace(' ', '').split(',')
+        
+        #
+        init_wire_string = '*Declared wires'
+        
+        for w in wires:
+            # get length of connection
+
+            # string
+            VA_line_str += 'X' + str(numberOfComponents) + ' ' + str(w) + '_0 ' + str(w) + '_1 ' + 'EChannel length='
+
+            row = wireLenDF.loc[wireLenDF['wire'] == w]
+
+            wireLength = row.iloc[0,1]
+
+            VA_line_str += str(wireLength) + '\n'
+
+            outFile_sp
+
+            wireList[w] = 0
+
+            numberOfComponents += 1
+        #pass
+    elif vars == 'endmodule':
+        pass
+
+
+    # variable is a standard cell
+    else:
+        #line_var = line.lstrip().split(' ')[0]
+
+        # check if in library
+        if (library['Standard_Cell'].eq(vars)).any():
+            standardCell = vars[0]
+        
+            ioPara = ''.join(line.replace('  ', ' ').split(' ')[2:])
+
+            io_expression = False
+            connectionWord= False
+            portPhrase = ''
+            ports      = []
+
+            for char in ioPara:
+                if not io_expression and not connectionWord and char == '.':
+                    io_expression = True
+                elif io_expression and not connectionWord and char == '(':
+                    #Start connection word
+                    io_expression = False
+                    connectionWord = True
+                elif not io_expression and connectionWord and char == ')':
+                    #end of port word
+                    io_expression = False
+                    connectionWord= False
+                    ports.append(portPhrase)
+                    portPhrase = ''
+                elif not io_expression and connectionWord:
+                    portPhrase += char
+
+            VA_line_str += 'X' + str(numberOfComponents) + ' '
+            VA_line_str_chem = ''
+
+            for p in ports:
+                if p in wireList.keys():
+                    VA_line_str += str(p) + '_' + str(wireList[p]) + ' '
+                    VA_line_str_chem += str(p) + '_' + str(wireList[p]) + 'c '
+                    wireList[p] += 1
+                else:
+                    VA_line_str += str(p) + ' '
+                    VA_line_str_chem += str(p) + 'c '
+
+                
+            VA_line_str += VA_line_str_chem + vars + '\n'
+
+            numberOfComponents += 1
+
+        else:
+            print("String: " + vars + " line number: " + str(line_num) + " not in library.")
+
+    # Write line to file
+    SPfile.write(VA_line_str)
+    # refresh line every pass
+    currentLine = ''
+
+SPfile.write(''.join(eExp.readlines()))
---
+++ smart_toilet.sp
@@ -0,0 +1,25 @@
+* Converted from verilog for microfluidic simulation
+
+.option post=1
+
+X0 soln1 soln1c PressurePump pressure=100k 
+X1 soln2 soln2c PressurePump pressure=100k 
+X2 soln3 soln3c PressurePump pressure=100k 
+
+X3 connect1_0 connect1_1 EChannel length=1.18
+X4 connect2_0 connect2_1 EChannel length=1.18
+X5 connect3_0 connect3_1 EChannel length=2.15
+X6 connect4_0 connect4_1 EChannel length=3.27
+X7 connect5_0 connect5_1 EChannel length=15.05
+X8 connect6_0 connect6_1 EChannel length=4.41
+X9 connect7_0 connect7_1 EChannel length=2.68
+X10 soln2 connect1_0 soln2c connect1_0c serpentine_50px_0
+X11 connect1_1 connect2_0 connect1_1c connect2_0c serpentine_150px_0
+X12 soln1 connect2_1 connect3_0 soln1c connect2_1c connect3_0c diffmix_25px_0
+X13 soln3 connect4_0 soln3c connect4_0c serpentine_300px_0
+X14 connect4_1 connect5_0 connect4_1c connect5_0c serpentine_300px_0
+X15 connect5_1 connect6_0 connect5_1c connect6_0c serpentine_300px_0
+X16 connect3_1 connect6_1 connect7_0 connect3_1c connect6_1c connect7_0c diffmix_25px_0
+X17 connect7_1 out connect7_1c outc serpentine_300px_0
+
+.tran 0.01ms 1ms
