5ba32f9b957cf0d3d586261916f331945747d649
bgoenner <bradygoenner@gmail.com>
2022-09-08 22:56:08 -0600

    can convert tr to csv

--- spiceExtract.py
+++ spiceExtract.py
@@ -2,8 +2,27 @@
 #import PySpice as sp
 
 from hspiceParser import import_export
+import numpy as np
+import pandas as pd
 
-inputFile = "./smart_toilet_soln1/smart_toilet_soln1_o.tr0"
-outputFile= "./smart_toilet_soln1/smart_toilet_soln1_o"
+TRdata = []
+TR_c   = []
 
-import_export(inputFile, outputFile)
+outputC = " v_outc"
+
+for i in range(1,4):
+    inputFileBase = "./smart_toilet_soln"+str(i)+"/smart_toilet_soln" + str(i) + "_o.tr0"
+    csvFile= "./smart_toilet_soln"+str(i)+"/smart_toilet_soln" + str(i) + "_o_tr0.csv"
+
+    import_export(inputFileBase, "csv")
+
+    df = pd.read_csv(csvFile, delimiter = ",")
+
+    TRdata.append(df)
+
+    print(df.columns)
+
+    TR_c.append(df.loc[:0, outputC])
+
+    
+pass
